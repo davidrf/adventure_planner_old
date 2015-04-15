f5285c5ec4eedbc802d35922e956e012ee00d866
David Rodriguez Fuentes <davidrf09@gmail.com>
2015-04-15 16:29:13 -0400

    Fix hound comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'adventures#index'
-  resources :adventures, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+  resources :adventures do
     resources :adventure_memberships, only: [:create]
   end
   devise_for :users
