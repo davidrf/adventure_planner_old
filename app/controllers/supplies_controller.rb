cf97e56eebecad2eab7f1caea0bf32321fd40f96
David Rodriguez Fuentes <davidrf09@gmail.com>
2015-04-21 14:27:13 -0400

    Implement supply create

--- app/controllers/supplies_controller.rb
+++ app/controllers/supplies_controller.rb
@@ -1,2 +1,26 @@
 class SuppliesController < ApplicationController
+  def new
+    @adventure = Adventure.find(params[:adventure_id])
+    @supply = Supply.new
+  end
+
+  def create
+    @adventure = Adventure.find(params[:adventure_id])
+    @supply = Supply.new(supplies_params)
+    @supply.adventure = @adventure
+
+    if @supply.save
+      flash[:notice] = "Supply Added!"
+      redirect_to adventure_url(@adventure)
+    else
+      flash[:alert] = "Supply Not Added. Invalid Form Submission."
+      render :new
+    end
+  end
+
+  private
+
+  def supplies_params
+    params.require(:supply).permit(:name)
+  end
 end
